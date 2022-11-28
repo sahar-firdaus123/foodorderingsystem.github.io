94946405bd876d8b489d2f34f9b0739347943485
Khushi Sharma <119272720+sahar-firdaus123@users.noreply.github.com>
2022-11-28 08:17:26 +0530

    Update

---
+++ Foods_Ordering/wsgi.py
@@ -0,0 +1,19 @@
+"""
+WSGI config for Foods_Ordering project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+path=os.path.expanduser('~/django_projects/Online_Food_Ordering_System/Online_Food_Ordering_System')
+if path not in sys.path:
+    sys.path.insert(0,path)
+os.environ['DJANGO_SETTINGS_MODULE']='Foods_Ordering.settings'
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
